993dc4a75b72fe9bcbd6dcd76af39dc486e8359c
AhadSiddiki <ahad.siddiki25@gmail.com>
2025-11-17 11:06:28 +0600

    Lab-2 Assign.. 1. update task

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const port = 3000;
 
 // In-memory storage
 const tasks = [
-  { id: 1, title: 'Sample Task', completed: false }
+  { id: 1, title: 'Complete Lab 2', completed: false, priority: 'high', createdAt: new Date() },
+  { id: 2, title: 'Repo Documentation', completed: false, priority: 'medium', createdAt: new Date() },
+  { id: 3, title: 'Gym Session', completed: false, priority: 'low', createdAt: new Date() },
+  { id: 4, title: 'Learn Nodejs', completed: true, priority: 'high', createdAt: new Date() },
+  { id: 5, title: 'create READme', completed: false, priority: 'medium', createdAt: new Date() }
 ];
 
 app.locals.tasks = tasks; 
