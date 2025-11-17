626411921619872a008e5cedd90e1e8b946e9d66
AhadSiddiki <ahad.siddiki25@gmail.com>
2025-11-17 11:38:37 +0600

    Lab-2 Assign.. 4. tasks move to tasks.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const taskRouter = require('./routes/tasks');
-
+const taskRoutes = require('./routes/tasks');
 const app = express();
 const port = 3000; 
 
-// In-memory storage
-const tasks = [
-  { id: 1, title: 'Complete Lab 2', completed: false, priority: 'high', createdAt: new Date() },
-  { id: 2, title: 'Repo Documentation', completed: false, priority: 'medium', createdAt: new Date() },
-  { id: 3, title: 'Gym Session', completed: false, priority: 'low', createdAt: new Date() },
-  { id: 4, title: 'Learn Nodejs', completed: true, priority: 'high', createdAt: new Date() },
-  { id: 5, title: 'create READme', completed: false, priority: 'medium', createdAt: new Date() }
-];
 
+// In-memory storage
+
+const tasks = []; 
 app.locals.tasks = tasks; 
 
 // JSON Parsing Middleware
@@ -23,20 +18,11 @@ app.get('/health', (req, res) => {
     uptime: process.uptime() // Returns server uptime in seconds
   });
 });
-// route/task/:id to get a task by its ID
-app.get('/task/:id', (req, res) => { 
-  const taskId = parseInt(req.params.id, 10); 
-  const task = tasks.find(t => t.id === taskId); 
-  if (task) { 
-    res.json(task); 
-  } else { 
-    res.status(404).json({ error: "Task not found" }); 
-  }
-});
+
 
 // Mount Router
 app.use('/tasks', taskRouter);
-
+app.use('/', taskRoutes); 
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const tasks = [
+  { id: 1, title: 'Complete Lab 2', completed: false, priority: 'high', createdAt: new Date() },
+  { id: 2, title: 'Repo Documentation', completed: false, priority: 'medium', createdAt: new Date() },
+  { id: 3, title: 'Gym Session', completed: false, priority: 'low', createdAt: new Date() },
+  { id: 4, title: 'Learn Nodejs', completed: true, priority: 'high', createdAt: new Date() },
+  { id: 5, title: 'create READme', completed: false, priority: 'medium', createdAt: new Date() }
+];
+
 // GET /tasks - Retrieve all tasks
 router.get('/', (req, res) => {
   const tasks = req.app.locals.tasks;
@@ -43,6 +51,16 @@ router.post('/', (req, res) => {
       success: false,
       error: 'Internal server error'
     });
+  }
+});
+
+router.get('/task/:id', (req, res) => { 
+  const taskId = parseInt(req.params.id, 10); 
+  const task = tasks.find(t => t.id === taskId); 
+  if (task) { 
+    res.json(task); 
+  } else { 
+    res.status(404).json({ error: "Task not found" }); 
   }
 });
 
