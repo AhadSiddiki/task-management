e5ef2477e69a8f5660797b9b878e13b81f87af7e
AhadSiddiki <ahad.siddiki25@gmail.com>
2025-11-17 11:20:28 +0600

    Lab-2 Assign.. 3. add route'/task/id

--- src/index.js
+++ src/index.js
@@ -23,6 +23,17 @@ app.get('/health', (req, res) => {
     uptime: process.uptime() // Returns server uptime in seconds
   });
 });
+// route/task/:id to get a task by its ID
+app.get('/task/:id', (req, res) => { 
+  const taskId = parseInt(req.params.id, 10); 
+  const task = tasks.find(t => t.id === taskId); 
+  if (task) { 
+    res.json(task); 
+  } else { 
+    res.status(404).json({ error: "Task not found" }); 
+  }
+});
+
 // Mount Router
 app.use('/tasks', taskRouter);
 
